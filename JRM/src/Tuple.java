03f20cb1bdf4d914b1c48d8182b5a0fc14449cc0
dave <dave@armchair.mb.ca>
2015-04-10 11:56:19 +0100

    Tweak formatting and add @OverrideS.

--- JRM/src/Tuple.java
+++ JRM/src/Tuple.java
@@ -1,20 +1,27 @@
 
-
 public class Tuple implements Comparable<Tuple> {
 	private String contents;
+
 	public Tuple(String contents) {
 		this.contents = contents;
 	}
+
+	@Override
 	public String toString() {
 		return "TUPLE {" + contents + "}";
 	}
+
+	@Override
 	public boolean equals(Object o) {
-		return contents.equals(((Tuple)o).contents);
+		return contents.equals(((Tuple) o).contents);
 	}
+
 	@Override
 	public int compareTo(Tuple o) {
 		return contents.compareTo(o.contents);
 	}
+
+	@Override
 	public int hashCode() {
 		return contents.hashCode();
 	}
